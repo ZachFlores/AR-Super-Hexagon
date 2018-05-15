ca3c740c83801cbf20fb70e1655d7363162ecd53
ZachFlores <zachflores1998@gmail.com>
2018-05-15 14:37:46 -0700

    Initial commit with some code and assets

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Rotate(Vector3.up * Time.deltaTime * speed * -1);
+        }
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hardcoded the lose function for right now");
+        Destroy(gameObject);
+    }
+}
